0efd6e89c019181c27f259369a38dc4670192952
xianpeiZ <xianpeizhang@qq.com>
2018-08-24 19:06:36 +0800

    Started pythonlearn project.

---
+++ python_work/hello_world.py
@@ -0,0 +1,7 @@
+def build_person(first_name, last_name, age=''):
+	person = {'first': first_name, 'last': last_name}
+	if age:
+		person['age'] = age
+	return person
+zxp = build_person('xianpei', 'zhang',22)
+print(zxp)
