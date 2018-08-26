d177bffb89670289fa665ef461e05ccb6f76531a
xianpeiZ <xianpeizhang@qq.com>
2018-08-26 16:18:03 +0800

    添加了game_stats.py，游戏初步可玩

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from game_stats import GameStats
 
 def run_game():
 	#初始化游戏并创建一个屏幕对象
@@ -12,6 +13,9 @@ def run_game():
 	ai_settings = Settings()
 	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien invasion")
+	
+	# 创建一个用于存储游戏统计信息的实例
+	stats = GameStats(ai_settings)
 	
 	# 创建一艘飞船
 	ship = Ship(ai_settings, screen)
@@ -22,7 +26,7 @@ def run_game():
 	aliens = Group()
 	
 	# 创建外星人群
-	gf.creat_fleet(ai_settings, screen, ship, aliens)
+	gf.create_fleet(ai_settings, screen, ship, aliens)
 	
 	#游戏的主循环
 	while True:
@@ -30,9 +34,11 @@ def run_game():
 		#监视键盘和鼠标事件
 		gf.check_events(ai_settings, screen, ship, bullets)
 		
-		ship.update()
-		gf.update_bullets(bullets)
-		gf.update_aliens(ai_settings, aliens)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+			gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+			
 		gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
